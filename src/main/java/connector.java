03bee3272d00d648dcbed3ed24cde2c7f5992064
Alejandro Berrio <alejandro.berrio@rappi.com>
2020-05-29 22:19:23 -0500

    correct connection to sql and structure webpage with css

--- src/main/java/connector.java
+++ src/main/java/connector.java
@@ -1,17 +1,28 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
 public class connector {
 
+    static ResultSet result;
+    static Statement statement;
+
     public static void coneccioner(){
-        String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=CAMPUS_PARTY;user=sa;password=your_password";
-        //String url ="jdbc:sqlserver://PC01\inst01;databaseName=DB01";
-        //String url ="jdbc:sqlserver://DESKTOP-GS71AU6//SQLEXPRESS;databaseName=CAMPUS_PARTY;integratedSecurity=true";
+        String connectionUrl = "jdbc:sqlserver://localhost:1433;database=CAMPUS_PARTY;user=usuarioSQL;password=abc";
+        //String connectionUrl ="jdbc:sqlserver://PC01\inst01;databaseName=DB01";
+        //String connectionUrl = "jdbc:sqlserver://localhost:1433;databaseName=CAMPUS_PARTY;integratedSecurity=true";
 
         System.out.print("Connecting to SQL Server ... ");
         try {
+
                 Connection connection = DriverManager.getConnection(connectionUrl);
+                statement = connection.createStatement();
+                result = statement.executeQuery("select * from Campusero");
                 System.out.println("Done.");
+            while (result.next()) {
+                System.out.println(result.getString(2));
+            }
 
         } catch (Exception e) {
             System.out.println();
--- web/index.jsp
+++ web/index.jsp
@@ -9,8 +9,16 @@
 <html>
   <head>
     <title>$Title$</title>
+    <link href="style.css" type="text/css" rel="stylesheet">
   </head>
   <body>
-  $END$
+  <div class="campusero-info">1</div>
+  <div class="info-equipo">2</div>
+  <div class="historial-equipos">3</div>
+  <div class="numero-equipos">4</div>
+  <div class="lista-fabricantes">5</div>
+  <div class="lista-software-educativo">6</div>
+  <div class="info-computador">7</div>
+  <div class="lista-software-gamer">8</div>
   </body>
 </html>
